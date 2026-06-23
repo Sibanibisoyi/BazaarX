7b0f568aa8aa0a6c30c08e5d7ab6c11dbbe6ceb0
Sibanibisoyi <bisoyisibani276@gmail.com>
2026-06-23 10:02:11 +0530

    connect railway mysql

--- bazarx/settings.py
+++ bazarx/settings.py
@@ -94,14 +94,15 @@ WSGI_APPLICATION = 'bazarx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bazarx_db',
-        'USER': 'bazarx_user',
-        'PASSWORD': 'bazarx123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT'),
     }
 }
 
